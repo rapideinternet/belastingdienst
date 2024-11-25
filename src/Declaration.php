c4dc38ae9b7979f6166d872141bd29a3a24c19a4
Rens Reinders <rens@rapide.software>
2024-11-25 19:43:57 +0100

    fix: added more shifted quarter tests, added a year caclulator for shifted "gebroken boekjaar"

--- src/Declaration.php
+++ src/Declaration.php
@@ -7,7 +7,6 @@ namespace Mijnkantoor\Belastingdienst;
 
 class Declaration
 {
-    public const IBAN = 'NL86INGB0002445588';
     public $declarationId;
     public $paymentReference;
     public $paymentDueDate;
@@ -18,21 +17,5 @@ class Declaration
         $this->declarationId = $declarationId;
         $this->paymentReference = $paymentReference;
         $this->paymentDueDate = $paymentDueDate;
-    }
-
-    public function getFormattedIban(): string
-    {
-        return sprintf('%2s %2d %4s %4s %4s %2s',
-            substr(self::IBAN, 0, 2),
-            substr(self::IBAN, 2, 2),
-            substr(self::IBAN, 4, 4),
-            substr(self::IBAN, 8, 4),
-            substr(self::IBAN, 12, 4),
-            substr(self::IBAN, 16, 2));
-    }
-
-    public function getFormattedPaymentReference(): string
-    {
-        return implode('.', str_split($this->paymentReference, 4));
     }
 }
